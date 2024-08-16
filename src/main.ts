efb250966721cb5b3adf6fd91897313c7be9c4a9
ume <ekadaumeonisofien@yahoo.com>
2024-08-16 21:09:52 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { MessagesModule } from './messages/messages.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  console.log("hello")
+  await app.listen(3000);
+}
+bootstrap();
